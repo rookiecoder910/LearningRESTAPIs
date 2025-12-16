6dd0f07cae74f4a2227fcf2de9e428d8c3f0c420
rookiecoder910 <carnageinside192@gmail.com>
2025-12-16 23:24:44 +0530

    dto setup

--- src/main/java/com/codingshuttle/youtube/LearningRESTAPIs/controller/StudentController.java
+++ src/main/java/com/codingshuttle/youtube/LearningRESTAPIs/controller/StudentController.java
@@ -8,6 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class StudentController {
 @GetMapping("/student")
     public StudentDto getStudent(){
-    return new StudentDto(4l,"Manas","manasop21@gmail.com");
+    return new StudentDto(1L,"Manas","manasop21@gmail.com");
+}
+@GetMapping("/student/{id}")
+    public StudentDto getStudentById(){
+    return new StudentDto(1L,"Manas","manasop21@gmail.com");
+
 }
 }
